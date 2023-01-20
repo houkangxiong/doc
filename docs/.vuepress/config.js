7dd7f318086e322a614b05870ba0bb7a534a62d2
hkx06 <3122272818@qq.com>
2023-01-20 16:16:15 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+// .vuepress/config.js
+module.exports = {
+  title: "繁夏*忆汝",
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/kenan.png' }],
+    ['meta', { name: 'author', content: '繁夏*忆汝' }]],
+  themeConfig: {
+    lastUpdated:'更新时间',
+    logo: '/assets/img/kenan.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '导航', link: '/guide/guide.md' },
+      { text: 'java', link: '/java/' },
+      { text: 'github', link: 'https://github.com/HickeyHou?tab=repositories' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'Japanese', link: '/language/japanese' }
+        ]
+      }
+    ],
+    sidebar: {
+      '/java/': [
+        'elasticsearch',
+        'rabbitmq'
+      ]
+    }
+  }
+}
